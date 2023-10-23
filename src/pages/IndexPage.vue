077391201ace669e20e8a4a8804f6b0a0b087460
Vladimir Sitchikhin <vsitchikhin@gmail.com>
2023-10-23 17:53:12 +0300

    feat(quasar_refactor): Миксин для работы с бэкраундом
    * Добавлен миксин для работы с размытием и прозрачностью бэкграунда

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,10 @@
 <template>
-  <q-page class="row items-center justify-evenly" />
+  <q-page class="row items-center justify-evenly">
+    <div class="container-test">
+      <h1>test container</h1>
+      <div class="wrapper-test">test wrapper</div>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
@@ -13,3 +18,33 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped lang="scss">
+@import 'src/css/app';
+
+.container-test {
+  position: relative;
+  background-color: $main;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+
+  h1 {
+    color: $main-text;
+    font-size: 200px;
+    line-height: 240px;
+    font-weight: 900;
+  }
+}
+
+.wrapper-test {
+  @include background-blur-opacity($secondary, 0.4, 20);
+  width: 80vw;
+  height: 200px;
+  position: absolute;
+  top: calc(50% - 100px);
+  left: 10vw;
+}
+</style>
